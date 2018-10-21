d53edd1ba8997e8bc45ed56fc21b11d24dafef5d
Georgios Tzourmpakis <kiougar@gmail.com>
2018-10-21 19:21:08 +0200

    v0.8.2
    Fixed compatibility with 18.06 `form`.
    
    Closes #26

--- luci-wrtbwmon/luasrc/controller/wrtbwmon.lua
+++ luci-wrtbwmon/luasrc/controller/wrtbwmon.lua
@@ -4,7 +4,7 @@ function index()
     entry({"admin", "network", "usage"}, alias("admin", "network", "usage", "details"), _("Usage"), 60)
     entry({"admin", "network", "usage", "details"}, template("wrtbwmon"), _("Details"), 10).leaf=true
     entry({"admin", "network", "usage", "config"}, cbi("wrtbwmon/config"), _("Configuration"), 20).leaf=true
-    entry({"admin", "network", "usage", "custom"}, cbi("wrtbwmon/custom"), _("User file"), 30).leaf=true
+    entry({"admin", "network", "usage", "custom"}, form("wrtbwmon/custom"), _("User file"), 30).leaf=true
     entry({"admin", "network", "usage", "check_dependency"}, call("check_dependency")).dependent=true
     entry({"admin", "network", "usage", "usage_data"}, call("usage_data")).dependent=true
     entry({"admin", "network", "usage", "usage_reset"}, call("usage_reset")).dependent=true

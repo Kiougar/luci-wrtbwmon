1daa63ee3f2727139f802a7594b41622c08a2f59
Georgios Tzourmpakis <kiougar@gmail.com>
2018-10-21 22:29:41 +0200

    Set version automatically from tag name
    If TRAVIS_TAG is set, use that value to set
    the version before creating the package

--- mkipk.sh
+++ mkipk.sh
@@ -4,6 +4,8 @@ tmpdir=`mktemp -d`
 mkdir -p $tmpdir/usr/lib/lua/luci && rsync -a luci-wrtbwmon/luasrc/ $_
 rsync -a luci-wrtbwmon/root/ $tmpdir
 rsync -a luci-wrtbwmon/htdocs/ $tmpdir/www
+# this automatically sets the version based on the tag name, if it exists
+[[ ! -z "${TRAVIS_TAG}" ]] && sed -i "s/Version: .*/Version: ${TRAVIS_TAG}/" CONTROL/control
 rsync -a CONTROL $tmpdir
 chmod a+x ipkg-build
 fakeroot -- ./ipkg-build -c $tmpdir

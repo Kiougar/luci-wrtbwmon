b3e6eed930c8a323c6cd1b0b48efe57407a4a8e4
Georgios Tzourmpakis <kiougar@gmail.com>
2019-02-07 18:46:59 +0100

    Fix mkipk.sh script

--- mkipk.sh
+++ mkipk.sh
@@ -5,7 +5,9 @@ mkdir -p $tmpdir/usr/lib/lua/luci && rsync -a luci-wrtbwmon/luasrc/ $_
 rsync -a luci-wrtbwmon/root/ $tmpdir
 rsync -a luci-wrtbwmon/htdocs/ $tmpdir/www
 # this automatically sets the version based on the tag name, if it exists
-[[ ! -z "${TRAVIS_TAG}" ]] && sed -i "s/Version: .*/Version: ${TRAVIS_TAG}/" CONTROL/control
+if [[ ! -z "${TRAVIS_TAG}" ]]; then
+    sed -i "s/Version: .*/Version: ${TRAVIS_TAG}/" CONTROL/control
+fi
 rsync -a CONTROL $tmpdir
 chmod a+x ipkg-build
 fakeroot -- ./ipkg-build -c $tmpdir
